db8cb61d1f010fb0189d24c891ce5cd75ad71ee0
Nghia Tran <nghiattran3@gmail.com>
2016-10-06 20:34:53 -0400

    update runcode

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,42 @@
 console.log(document.getElementById('code-editor'));
 
-var editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
-	lineNumbers: true,
-	matchBrackets: true,
-	lineWrapping: true,
-	scrollbarStyle: 'native',
-	lineWrapping: true,
-	theme: 'icecoder',
-	extraKeys: {'Enter': 'newlineAndIndentContinueMarkdownList'},
-	mode: 'text/x-c++src'
-}, { showToolbar: true });
-
+var editors = [];
+init();
 function changeLanguage(language) {
-	editor.setOption("mode", language);
+	for (var i = 0; i < editors.length; i++) {
+		editors[i].setOption("mode", language);
+	}
 }
 
 $('#languageSelector').on('change', function (e) {
 	changeLanguage($(this ).val());
 })
+
+function init() {
+	editors.push(createCodeMirror('code-editor'));
+	createCodeMirror('code-output', {
+		readOnly: 'nocursor',
+		showCursorWhenSelecting: false
+	})
+}
+
+function createCodeMirror(id, options) {
+	options = options || {};
+
+	var defaultOptions = {
+		lineNumbers: true,
+		matchBrackets: true,
+		lineWrapping: true,
+		scrollbarStyle: 'native',
+		lineWrapping: true,
+		theme: 'icecoder',
+		extraKeys: {'Enter': 'newlineAndIndentContinueMarkdownList'},
+		mode: 'text/x-c++src',
+	}
+
+	for (var key in options) {
+		defaultOptions[key] = options[key];
+	}
+
+	return CodeMirror.fromTextArea(document.getElementById(id), defaultOptions, { showToolbar: true })
+}
